51a4d292fd6a533808a2757d1f0d51884d3cbf89
Nkurunziza Innocent <i.nkurunziz@alustudent.com>
2026-09-12 19:21:51 +0200

    m

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -25,8 +26,11 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* Function prototypes */
+/* Visualization helper */
 void binary_tree_print(const binary_tree_t *);
+
+/* Task Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
